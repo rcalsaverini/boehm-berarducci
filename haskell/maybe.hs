596bc104e4530501409ba1d52c11c801e82ba137
Rafael Calsaverini <rafael.calsaverini@gmail.com>
2016-10-11 18:37:38 -0300

    Update maybe.hs
    Created Maybe type on Haskell

---
+++ haskell/maybe.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE RankNTypes #-}
+
+import Prelude hiding (Maybe, Just, Nothing, maybe)
+
+data Maybe a = Maybe {maybe :: forall r . r -> (a -> r) -> r}
+
+nothing = Maybe (\nothing' just' -> nothing')
+just x  = Maybe (\nothing' just' -> just' x)
+
+instance (Show a) => (Show (Maybe a)) where
+  show xs = maybe xs "Nothing" (\x -> "Just " ++ show x)
+  
+instance Functor Maybe where
+  fmap f xs = maybe xs nothing (\us -> just $ f us)
+  
+instance Applicative Maybe where
+  pure = just
+  fs <*> xs = maybe fs nothing (\f -> fmap f xs)
+
+instance Monad Maybe where
+  xs >>= f = maybe xs nothing f
+
+safeHead [] = nothing
+safeHead (x:xs) = just x
+
+main = print $ just "omobó" >>= safeHead
