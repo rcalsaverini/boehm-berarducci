d5c6d9b2676d61b8283bc70f4dc451f0f6e489da
Rafael S. Calsaverini! <rafael.calsaverini@gmail.com>
2016-10-22 00:28:56 -0200

    added free for python

---
+++ python/free.py
@@ -0,0 +1,44 @@
+from functools import wraps
+
+
+class Free(object):
+
+    def __init__(self, unfree):
+        self.unfree = unfree
+
+    def __call__(self, pureF, freeF):
+        return self.unfree(pureF, freeF)
+
+    def __rshift__(self, function):
+        return self(function, free)
+
+    def __rmul__(self, xs):
+        return self(xs.map, free)
+
+    def __rand__(self, function):
+        return self >> (lambda x: pure(function(x)))
+
+    def map(self, function):
+        return function & self
+
+    def join(self):
+        self >> (lambda x: x)
+
+    def iter(function):
+        return self(lambda x: x, function)
+
+    def interpret(function):
+        return self(pure, lambda x: self.join(function(x)))
+
+
+
+def pure(x):
+    return Free(lambda pureF, freeF: pureF(x))
+
+def free(fs):
+    def unfree(pureF, freeF):
+        freeF(fs.map(lambda f: f(pureF, freeF)))
+    return Free(unfree)
+
+def liftF(fs):
+    free(fs.map(pure))
